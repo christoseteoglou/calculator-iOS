7b0b2d88e4531bee14bf05214d11d28e5dcb4f1a
Christos Eteoglou <itsmeglue@gmail.com>
2023-06-26 13:16:49 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Christos Eteoglou on 2023-06-26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
